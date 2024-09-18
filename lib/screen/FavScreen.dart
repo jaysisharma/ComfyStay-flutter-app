e3d9be11fccaa30e22b17723e88f513715918e56
bhurendra31 <bhurendra31@gmail.com>
2024-09-18 10:32:41 +0530

    FavScreen added

---
+++ lib/screen/FavScreen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Favscreen extends StatelessWidget {
+  const Favscreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
