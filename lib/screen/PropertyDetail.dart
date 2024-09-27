602715760e82d033c1929bb5dd3d3a32f75ced1a
jaysi <jaysi@kali.jaysi>
2024-09-27 23:35:32 +0530

    PropertyDetails Page Modified

--- lib/screen/PropertyDetail.dart
+++ lib/screen/PropertyDetail.dart
@@ -7,7 +7,9 @@ class PropertyDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("ss"),
+      body: Column(children: [
+        Text("Ss")
+      ],),
     );
   }
 }
