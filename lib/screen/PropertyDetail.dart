37488362d6ce629ca9f59f01e4618540328ca516
jaysi <jaysi@kali.jaysi>
2024-09-28 13:23:09 +0530

    PropertyDetails Page Modified

--- lib/screen/PropertyDetail.dart
+++ lib/screen/PropertyDetail.dart
@@ -7,9 +7,59 @@ class PropertyDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [
-        Text("Ss")
-      ],),
+      appBar: AppBar(
+        leading: Icon(Icons.arrow_back_ios),
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: 400,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                    "assets/images/roomcard.jpeg"), // Replace with your image path
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 18.0, vertical: 18),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Rs 15,000/month",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    Icon(Icons.share)
+                  ],
+                ),
+                 SizedBox(
+                  height: 15,
+                ),
+                Text("Royal Villa", style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                SizedBox(
+                  height: 15,
+                ),
+                Text(
+                  "A villa with a swimming pool and a premium feel, offering ultimate luxury and comfort.",
+                  style: TextStyle(fontWeight: FontWeight.w600),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text("Ammenties", style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),)
+
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
